a6f90a7e169d3dc22b3c6d5eedc9bdc2d561609a
lubentancor <bentancorlucia@gmail.com>
2023-12-06 19:18:47 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// hamburguer menu
+
+let navLinks = document.querySelector(".navbar_links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".navbar_links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".inst-arrow");
+htmlcssArrow.onclick = function() {
+navLinks.classList.toggle("show1");
+}
+
+let jsArrow = document.querySelector(".socios-arrow");
+jsArrow.onclick = function() {
+navLinks.classList.toggle("show2");
+}
+
+// swiper supervisores
+
+const swiper = new Swiper('.swiper_supervisores', {
+  roundLengths: true,
+  loop: false,
+  spaceBetween:20,
+  mode: 'horizontal',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  slideToClickedSlide: true,
+  preventClicks: true,
+  centeredSlides: true,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.2,
+      spaceBetween: 30
+    },
+
+    1000: {
+      slidesPerView: 2.5,
+      spaceBetween: 30
+    },
+  }
+  });
